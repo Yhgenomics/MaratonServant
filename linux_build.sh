4e297a016d7b7f3533a44d2a105b0cbe3c1584b7
BigCatRoyalYang <royalyk@126.com>
2016-04-20 15:55:05 +0800

    adjust for ubuntu

--- linux_build.sh
+++ linux_build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf cmakebuild
-CMaker project:maraton-servant compiler:clang++ head:*.h src:*.cpp,*.c,*.cc,*.hpp lib:/usr/local/lib/libprotobuf.a,./lib/libmaraton.a,./lib/libuv.a
+CMaker project:maraton-servant compiler:clang++ head:*.h src:*.cpp,*.c,*.cc,*.hpp lib:/usr/local/lib/libprotobuf.a,./lib/libMaratonFramework.a,./lib/libuv.a
 cd cmakebuild
 cmake ./
 make -j 5
