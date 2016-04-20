88571b6a5d0ce573f67c276c30ef33b1d0793997
BigCatRoyalYang <royalyk@126.com>
2016-04-20 14:52:58 +0800

    update Framework

--- include/framework/HTTP.h
+++ include/framework/HTTP.h
@@ -18,9 +18,9 @@ limitations under the License.
 ***********************************************************************************/
 
 /***********************************************************************************
-* Description   : 
-* Creator       : 
-* Date          : 
+* Description   :
+* Creator       :
+* Date          :
 * Modifed       : When      | Who       | What
 ***********************************************************************************/
 
@@ -55,7 +55,7 @@ public:
 
     Url                      ( std::string url );
     ~Url                     ();
-                             
+
     std::string Domain       ();
     std::string Path         ();
     std::string Protocol     ();
@@ -75,7 +75,84 @@ private:
     int         port_     = 80;
     std::string tmp_      = "";
 
-    map<string,string> parameter_;
+    map<string , string> parameter_;
+
+    std::string UrlEncode( const std::string& szToEncode )
+    { 
+        std::string src = szToEncode;
+        char hex[] = "0123456789ABCDEF";
+        string dst;
+        for ( size_t i = 0; i < src.size(); ++i )
+        {
+            unsigned char cc = src[i];
+            if ( isascii( cc ) )
+            {
+                if ( cc == ' ' )
+                {
+                    dst += "%20";
+
+                }
+                else
+                    dst += cc;
+            }
+            else
+            {
+
+                unsigned char c = static_cast< unsigned char >( src[i] );
+                dst += '%';
+                dst += hex[c / 16];
+                dst += hex[c % 16];
+            }
+
+        }
+        return dst;
+    }
+    std::string UrlDecode( const std::string& szToDecode )
+    {
+        std::string result;
+        int hex = 0;
+        for ( size_t i = 0; i < szToDecode.length(); ++i )
+        {
+            switch ( szToDecode[i] )
+            {
+                case '+':
+                    result += ' ';
+                    break;
+                case '%':
+                    if ( isxdigit( szToDecode[i + 1] ) && isxdigit( szToDecode[i + 2] ) )
+                    {
+                        std::string hexStr = szToDecode.substr( i + 1 , 2 );
+                        hex = strtol( hexStr.c_str() , 0 , 16 );
+                        //字母和数字[0-9a-zA-Z]、一些特殊符号[$-_.+!*'(),] 、以及某些保留字[$&+,/:;=?@] 
+
+                        //可以不经过编码直接用于URL  
+                        if ( !( ( hex >= 48 && hex <= 57 ) || //0-9  
+                             ( hex >= 97 && hex <= 122 ) ||   //a-z  
+                             ( hex >= 65 && hex <= 90 ) ||    //A-Z  
+                             //一些特殊符号及保留字[$-_.+!*'(),]  [$&+,/:;=?@]  
+                             hex == 0x21 || hex == 0x24 || hex == 0x26 || hex == 0x27 || hex == 0x28 || hex == 0x29
+                             || hex == 0x2a || hex == 0x2b || hex == 0x2c || hex == 0x2d || hex == 0x2e || hex == 0x2f
+                             || hex == 0x3A || hex == 0x3B || hex == 0x3D || hex == 0x3f || hex == 0x40 || hex == 0x5f
+                             ) )
+                        {
+                            result += char( hex );
+                            i += 2;
+                        }
+                        else result += '%';
+                    }
+                    else
+                    {
+                        result += '%';
+                    }
+                    break;
+                default:
+                    result += szToDecode[i];
+                    break;
+            }
+
+        }
+        return result;
+    }
 };
 // ===========================================
 // HTTPRequest
@@ -83,7 +160,7 @@ private:
 class HTTPRequest
 {
 public:
-     
+
     HTTPRequest ( std::string url ,
                   std::string method );
     HTTPRequest ();
@@ -96,11 +173,17 @@ public:
     uptr<Buffer> Content        ();
     void         ContentLength  ( size_t size );
     size_t       ContentLength  ();
-    
+
     void         Header         ( std::string key , std::string value );
     std::string  Header         ( std::string key );
-    std::string  Domain         (){ return this->domain_; };
-    std::string  Host           (){ return this->ip_;     };
+    std::string  Domain         ()
+    {
+        return this->domain_;
+    };
+    std::string  Host           ()
+    {
+        return this->ip_;
+    };
     std::string  RequestUrl     ();
 
     void Parse                  ( uptr<Buffer> data );
@@ -113,7 +196,7 @@ public:
 
     bool         Finish         ();
     sptr<Url>    Uri            ();
-     
+
 private:
 
     std::string                         ip_             = "";
@@ -131,11 +214,11 @@ private:
     // ==================================================================
     enum ParseState
     {
-        kMethod = 1,
-        kUrl,
-        kProtocol,
-        kHeadKey,
-        kHeadValue,
+        kMethod = 1 ,
+        kUrl ,
+        kProtocol ,
+        kHeadKey ,
+        kHeadValue ,
         kContent
     };
 
@@ -153,20 +236,20 @@ private:
 class HTTPResponse
 {
 public:
-     
+
     //HTTPResponse                 ( size_t Status );
     HTTPResponse                 ();
     ~HTTPResponse                ();
 
     void         ReadCallback    ( RepReadCallbackType callback );
-                                 
+
     void*        Data            ();
     void         Data            ( void* value );
-                                        
-    void         Header          ( std::string key , 
+
+    void         Header          ( std::string key ,
                                    std::string value );
     std::string  Header          ( std::string key );
-                 
+
     void         Content         ( uptr<Buffer> content );
     void         Content         ( std::string content );
     uptr<Buffer> Content         ();
@@ -183,7 +266,7 @@ public:
 
     bool         Finish          ();
 
-private: 
+private:
 
     size_t              status_         = 0;
     std::string         status_str_     = "";
@@ -193,25 +276,25 @@ private:
     void*               data_           = nullptr;
     uptr<Buffer>        content_        = nullptr;
     std::map<
-       std::string , 
-       std::string>     header_;
+        std::string ,
+        std::string>     header_;
 
     // Parsing used
     // ==================================================================
     enum ParseState
     {
-        kProtocol = 1,
-        kStatusCode,
-        kStatus,
-        kHeadKey,
-        kHeadValue,
+        kProtocol = 1 ,
+        kStatusCode ,
+        kStatus ,
+        kHeadKey ,
+        kHeadValue ,
         kContent
     };
-   
+
     ParseState   parse_state_ = ParseState::kProtocol;
     std::string  tmp_key_     = "";
     std::string  tmp_value_   = "";
-}; 
+};
 
 class WebRequestSession :
     public Session
@@ -219,16 +302,16 @@ class WebRequestSession :
 public:
 
     WebRequestSession           ( uptr<HTTPRequest> req );
-    ~WebRequestSession          ( );
+    ~WebRequestSession          ();
 
-    uptr<HTTPResponse> Response ( );
+    uptr<HTTPResponse> Response ();
 
 protected:
 
-    virtual void OnConnect      ()                  ;
+    virtual void OnConnect      ();
     virtual void OnRead         ( uptr<Buffer> data );
     virtual void OnWrite        ( uptr<Buffer> data );
-    virtual void OnClose        ()                  ;
+    virtual void OnClose        ();
 
 private:
 
